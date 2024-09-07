cc5428ae187345f3c9a6c6d80ba42b47cf55be9a
pety02 <pety02@abv.bg>
2024-09-07 10:48:29 +0300

    Init commit

---
+++ src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/FootbalPlayersSirmaAcademySeason4FinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.footbalplayers_sirmaacademyseason4_finalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbalPlayersSirmaAcademySeason4FinalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootbalPlayersSirmaAcademySeason4FinalExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/FootbalPlayersSirmaAcademySeason4FinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footbalplayers_sirmaacademyseason4_finalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootbalPlayersSirmaAcademySeason4FinalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
