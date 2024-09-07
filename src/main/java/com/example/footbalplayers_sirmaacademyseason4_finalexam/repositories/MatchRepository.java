c22f7434d96c6f57f86feb1eadcd3c3a1f431dbf
pety02 <pety02@abv.bg>
2024-09-07 22:38:43 +0300

    MatchRepository and RecordRepository classes refactored. Some TODOs added.

--- src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/repositories/MatchRepository.java
+++ src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/repositories/MatchRepository.java
@@ -4,6 +4,9 @@ import com.example.footbalplayers_sirmaacademyseason4_finalexam.models.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
+    Optional<Match> findById(Long id);
 }
--- src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/repositories/RecordRepository.java
+++ src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/repositories/RecordRepository.java
@@ -4,6 +4,9 @@ import com.example.footbalplayers_sirmaacademyseason4_finalexam.models.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    Optional<Record> findById(Long id);
 }
