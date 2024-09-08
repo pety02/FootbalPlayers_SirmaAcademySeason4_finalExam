f08401a608e88592288cee6b3478189dab0c4546
pety02 <pety02@abv.bg>
2024-09-08 11:52:49 +0300

    MatchConverter and RecordConverter class implmented and fully commented!

--- src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/converters/MatchConverter.java
+++ src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/converters/MatchConverter.java
@@ -2,31 +2,64 @@ package com.example.footbalplayers_sirmaacademyseason4_finalexam.converters;
 
 import com.example.footbalplayers_sirmaacademyseason4_finalexam.converters.interfaces.Convertable;
 import com.example.footbalplayers_sirmaacademyseason4_finalexam.dtos.MatchDTO;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.dtos.TeamDTO;
 import com.example.footbalplayers_sirmaacademyseason4_finalexam.models.Match;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.models.Team;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-// TODO: to comment the class
 @Component
 public class MatchConverter implements Convertable<Match, MatchDTO> {
+    private final TeamRepository teamRepository;
+    private final TeamConverter teamConverter;
+
     /**
-     *
-     * @param matchDTO
-     * @return
+     * MatchConverter class constructor with arguments
+     * @param teamRepository the team repository
+     * @param teamConverter the team converter
+     */
+    @Autowired
+    public MatchConverter(TeamRepository teamRepository,
+                          TeamConverter teamConverter) {
+        this.teamRepository = teamRepository;
+        this.teamConverter = teamConverter;
+    }
+    /**
+     * Converts a MatchDTO object to a Match object
+     * @param matchDTO the MatchDTO object
+     * @return a Match object
      */
     @Override
     public Match toEntity(MatchDTO matchDTO) {
-        // TODO: to implement it
-        return null;
+        Match match = new Match();
+        match.setId(matchDTO.getId());
+        Team teamA = teamRepository.findById(matchDTO.getATeamId()).orElse(null);
+        match.setATeam(teamA);
+        Team teamB = teamRepository.findById(matchDTO.getBTeamId()).orElse(null);
+        match.setBTeam(teamB);
+        match.setDate(matchDTO.getDate());
+        match.setScore(matchDTO.getScore());
+
+        return match;
     }
 
     /**
-     *
-     * @param match
-     * @return
+     * Converts a Match object to a MatchDTO object
+     * @param match the Match object
+     * @return a MatchDTO object
      */
     @Override
     public MatchDTO toDTO(Match match) {
-        // TODO: to implement it
-        return null;
+        MatchDTO matchDTO = new MatchDTO();
+        matchDTO.setId(match.getId());
+        TeamDTO teamADTO = teamConverter.toDTO(match.getATeam());
+        matchDTO.setATeamId(teamADTO.getId());
+        TeamDTO teamBDTO = teamConverter.toDTO(match.getBTeam());
+        matchDTO.setBTeamId(teamBDTO.getId());
+        matchDTO.setDate(match.getDate());
+        matchDTO.setScore(match.getScore());
+
+        return matchDTO;
     }
 }
--- src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/converters/RecordConverter.java
+++ src/main/java/com/example/footbalplayers_sirmaacademyseason4_finalexam/converters/RecordConverter.java
@@ -1,32 +1,77 @@
 package com.example.footbalplayers_sirmaacademyseason4_finalexam.converters;
 
 import com.example.footbalplayers_sirmaacademyseason4_finalexam.converters.interfaces.Convertable;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.dtos.MatchDTO;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.dtos.PlayerDTO;
 import com.example.footbalplayers_sirmaacademyseason4_finalexam.dtos.RecordDTO;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.models.Match;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.models.Player;
 import com.example.footbalplayers_sirmaacademyseason4_finalexam.models.Record;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.repositories.MatchRepository;
+import com.example.footbalplayers_sirmaacademyseason4_finalexam.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-// TODO: to comment the class
 @Component
 public class RecordConverter implements Convertable<Record, RecordDTO> {
+    private final MatchRepository matchRepository;
+    private final MatchConverter matchConverter;
+    private final PlayerRepository playerRepository;
+    private final PlayerConverter playerConverter;
+
     /**
-     *
-     * @param recordDTO
-     * @return
+     * RecordConverter class constructor with arguments
+     * @param matchRepository the match repository
+     * @param matchConverter the match converter
+     * @param playerRepository the player repository
+     * @param playerConverter the player converter
      */
-    @Override
-    public Record toEntity(RecordDTO recordDTO) {
-        // TODO: to implement it
-        return null;
+    @Autowired
+    public RecordConverter(MatchRepository matchRepository,
+                           MatchConverter matchConverter,
+                           PlayerRepository playerRepository,
+                           PlayerConverter playerConverter) {
+        this.matchRepository = matchRepository;
+        this.matchConverter = matchConverter;
+        this.playerRepository = playerRepository;
+        this.playerConverter = playerConverter;
     }
 
     /**
-     *
-     * @param record
-     * @return
+     * Converts a RecordDTO object to a Record object
+     * @param recordDTO the RecordDTO object
+     * @return a Record object
+     */
+    @Override
+    public Record toEntity(RecordDTO recordDTO) {
+        Record record = new Record();
+        record.setId(recordDTO.getId());
+        Match match = matchRepository.findById(recordDTO.getMatchId()).orElse(null);
+        record.setMatch(match);
+        Player player = playerRepository.findById(recordDTO.getPlayerId()).orElse(null);
+        record.setPlayer(player);
+        record.setFromMinutes(recordDTO.getFromMinutes());
+        record.setToMinutes(recordDTO.getToMinutes());
+
+        return record;
+    }
+
+    /**
+     * Converts a Record object to a RecordDTO object
+     * @param record the Record object
+     * @return a RecordDTO object
      */
     @Override
     public RecordDTO toDTO(Record record) {
-        // TODO: to implement it
-        return null;
+        RecordDTO recordDTO = new RecordDTO();
+        recordDTO.setId(record.getId());
+        MatchDTO matchDTO = matchConverter.toDTO(record.getMatch());
+        recordDTO.setMatchId(matchDTO.getId());
+        PlayerDTO playerDTO = playerConverter.toDTO(record.getPlayer());
+        recordDTO.setPlayerId(playerDTO.getId());
+        recordDTO.setFromMinutes(record.getFromMinutes());
+        recordDTO.setToMinutes(recordDTO.getToMinutes());
+
+        return recordDTO;
     }
 }
